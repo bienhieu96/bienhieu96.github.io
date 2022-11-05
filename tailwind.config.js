173daf4962857d03c09a372e46d0120bfb5ab4be
bienhieu96 <54875534+bienhieu96@users.noreply.github.com>
2022-11-05 13:49:36 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#283046",
+      },
+      backgroundColor: {
+        primary: {
+          default: "#283046",
+          600: "#282C38",
+        },
+      },
+      gridRow: {
+        12: "grid-template-rows: repeat(6, minmax(0, 1fr))",
+      },
+    },
+  },
+  plugins: [],
+};
